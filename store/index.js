bb232e711c78f4cd44fb779659c3e1d1efa7872c
Isaias Pomales <impomales@gmail.com>
2018-06-21 16:20:28 -0400

    feat(redux): connect game to main store

--- store/index.js
+++ store/index.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import player from './player';
-import game form './game';
+import game from './game';
 
 const reducer = combineReducers({ player, game });
 
@@ -10,5 +10,6 @@ const middleware = applyMiddleware(createLogger({ collapsed: true }));
 const store = createStore(reducer, middleware);
 
 export default store;
+
 export * from './player';
 export * from './game';
