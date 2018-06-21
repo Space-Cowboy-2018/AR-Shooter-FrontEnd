b67beccffd9a5fa07fc259a200f185bfcf0bc233
Isaias Pomales <impomales@gmail.com>
2018-06-21 16:17:04 -0400

    feat(redux): setup redux state for game info

---
+++ store/game.js
@@ -0,0 +1,6 @@
+export default function(state = { initialState }, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
--- store/index.js
+++ store/index.js
@@ -1,11 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import player from './player';
+import game form './game';
 
-const reducer = combineReducers({ player });
+const reducer = combineReducers({ player, game });
 
 const middleware = applyMiddleware(createLogger({ collapsed: true }));
 
 const store = createStore(reducer, middleware);
 
 export default store;
+export * from './player';
+export * from './game';
