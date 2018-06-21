237a52befe9e9fc9997cd48ba800f3205d2ad26b
Isaias Pomales <impomales@gmail.com>
2018-06-21 16:20:59 -0400

    feat(redux): define initial state of game

--- store/game.js
+++ store/game.js
@@ -1,3 +1,9 @@
+const initialState = {
+  targetPosition: {},
+  isOver: false,
+  timer: 60
+};
+
 export default function(state = { initialState }, action) {
   switch (action.type) {
     default:
