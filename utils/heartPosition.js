9c6f5890bc8b5137bfe9b657bd51d72391ce7511
richardsunny2015 <richardsunny2015@gmail.com>
2018-07-03 12:11:18 -0400

    chore(heartPosition): added comment in heartPosition.js

--- utils/heartPosition.js
+++ utils/heartPosition.js
@@ -3,6 +3,6 @@ export default function(playerPosition, heartPosition) {
     x: Math.abs(playerPosition.x - heartPosition.x),
     z: Math.abs(playerPosition.z - heartPosition.z)
   };
-  const buffer = 0.01;
+  const buffer = 0.01; // may remove if accurate enough
   return difference.x < buffer && difference.z < buffer;
 }
